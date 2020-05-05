e8254c921be8854511e481bc400aa8e340ba5e4b
Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
2020-05-05 15:00:08 +0200

    Release 0.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,10 @@
-## [0.1.0] - 2019-??-??
+## [0.0.1] - 2020-05-05
 
-* initial revision
+* initial revision;
 
+* low level application state processing;
+
+* application state management functionality;
+
+* component construction macro;
+ 
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject io.resonant/conjector "0.0.1-SNAPSHOT"
+(defproject io.resonant/conjector "0.0.1"
   :description "Dependency tree component processing library"
-  :url "http://resonant.io/conjector"
+  :url "http://resonant.io/projects/conjector"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies
