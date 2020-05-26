763d013faf191600e6b639e19667622b04aa3e06
Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
2020-05-26 07:30:29 +0200

    New iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.resonant/conjector "0.0.2"
+(defproject io.resonant/conjector "0.0.3-SNAPSHOT"
   :description "Dependency tree component processing library"
   :url "http://resonant.io/projects/conjector"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
